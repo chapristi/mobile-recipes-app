23fe1919946787b89749407f63b0405ceb365fc9
chapristi <70172942+chapristi@users.noreply.github.com>
2022-12-24 18:02:54 +0100

    new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:untitled3/recipe.dart';
+import 'package:untitled3/recipeListScreen.dart';
 import 'package:untitled3/recipeScreen.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -15,20 +16,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      initialRoute: "/" ,
+      onGenerateRoute: (settings) => RouterGenerator.generateRoute(settings),
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
       ),
-      home: RecipeScreen(
-        recipe: Recipe(
-          "Pizza facile",
-          "Proposé par Louis",
-          "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
-          "1- Faire cuire ....\n2- Préchauffez le four à 180°c.\n3- Otez la croûte des tranches de pain. Tapissez en le fond d'un moule rectangulaire 20x30 .\n4- Dans un saladier, fouettez les œufs avec le lait et le fromage blanc. ",
-          false,
-          50)),
+
     );
   }
 }
 
+class RouterGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (context) => RecipeListScreen());
+      case "/recipe":
+        return PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                RecipeScreen(recipe: settings.arguments as Recipe),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              animation =
+                  CurvedAnimation(parent: animation, curve: Curves.ease);
+              return FadeTransition(opacity: animation, child: child);
 
+              /*
+                      var begin = Offset(0.0, 1.0);
+                      var end = Offset.zero;
+                      var curve= Curves.linearToEaseOut;
+                      var tween = Tween(begin: begin,end: end).chain(CurveTween(curve: curve));
+                      return SlideTransition(
+                          position: animation.drive(tween),
+                          child : child
+                     );
+
+                   */
+            });
+      default:
+        return MaterialPageRoute(
+            builder: (context) => Scaffold(
+                appBar: AppBar(title: const Text("Error"), centerTitle: true),
+                body: const Center(
+                  child: Text("Page not found"),
+                )));
+    }
+  }
+}
 
---
+++ lib/recipeListScreen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:untitled3/recipe.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:untitled3/recipeScreen.dart';
+
+class RecipeListScreen extends StatelessWidget {
+  final List<Recipe> recipes = [
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        false,
+        1444),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        50),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        false,
+        774),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+    Recipe(
+        "Pizza facile",
+        "Par Louis",
+        "https://www.cometemenorca.com/static/uploads/pizzas.jpg",
+        "faire cuire...",
+        true,
+        68),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Mes recettes "),
+        ),
+        body: ListView.builder(
+            itemCount: recipes.length,
+            itemBuilder: (context, index) {
+              return RecipeItemWidget(recipe: recipes[index]);
+            }));
+  }
+}
+
+class RecipeItemWidget extends StatelessWidget {
+  final Recipe recipe;
+  const RecipeItemWidget({
+    required this.recipe,
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, "/recipe", arguments: recipe);
+      },
+      child: Card(
+          margin: const EdgeInsets.all(8),
+          elevation: 8,
+          child: Row(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    child: const Center(child: CircularProgressIndicator()),
+                  ),
+                  FadeInImage.memoryNetwork(
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                    placeholder: kTransparentImage,
+                    image: recipe.imageUrl,
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        recipe.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                    Text(
+                      recipe.user,
+                      style: TextStyle(color: Colors.grey[500], fontSize: 16),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
